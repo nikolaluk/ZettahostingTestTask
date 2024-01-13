ff6931243b7f8af23d654c23a4552db6546843e4
CeoOfStoicism <nikola.lukov.2006@gmail.com>
2024-01-13 21:24:30 +0200

    Added a bunch of breakpoints for mobile devices

--- zettahosting-task/src/components/core/Filter/Filter.jsx
+++ zettahosting-task/src/components/core/Filter/Filter.jsx
@@ -7,32 +7,48 @@ import "./Filter.css"
 function Filter() {
 
     return (
-        <div className="filter-wrapper">
-            {/* Left filter content */}
-            <div className="filter-contents-left big">
-                <FilterTab data={{content: "All"}}></FilterTab>
-                <FilterTab data={{content: "Favourites"}}></FilterTab>
-                <FilterTab data={{content: "Popular"}}></FilterTab>
-                <FilterTab data={{content: "20% Cash Back"}}></FilterTab>
-                <FilterTabDropdown data={{content: "More"}}></FilterTabDropdown>
-            </div>
-            <div className="filter-contents-left small">
-                <FilterTabDropdown data={{content: "All"}}></FilterTabDropdown>
+        <>
+            <div className="filter-wrapper">
+                {/* Left filter content */}
+                <div className="filter-contents-left">
+                    <FilterTab data={{ content: "All" }}></FilterTab>
+                    <FilterTab data={{ content: "Favourites" }}></FilterTab>
+                    <FilterTab data={{ content: "Popular" }}></FilterTab>
+                    <FilterTab data={{ content: "20% Cash Back" }}></FilterTab>
+                    <FilterTabDropdown data={{ content: "More" }}></FilterTabDropdown>
+                </div>
+                <div className="filter-contents-left-small">
+                    <FilterTabDropdown data={{ content: "All" }}></FilterTabDropdown>
+                </div>
+
+                {/* Right filter content */}
+                <div className="filter-contents-right">
+                    <Dropdown data={{ label: "By provider" }}></Dropdown>
+                    <Dropdown data={{ label: "By genre" }}></Dropdown>
+                    <SearchInput></SearchInput>
+                </div>
+
+                <div className="filter-contents-right-small">
+                    <Dropdown data={{ label: "By provider", compact: true }}></Dropdown>
+                    <Dropdown data={{ label: "By genre", compact: true }}></Dropdown>
+                    <SearchInput></SearchInput>
+                </div>
             </div>
 
-            {/* Right filter content */}
-            <div className="filter-contents-right big">
-                <Dropdown data={{label: "By provider"}}></Dropdown>
-                <Dropdown data={{label: "By genre"}}></Dropdown>
-                <SearchInput></SearchInput>
-            </div>
+            <div className="filter-wrapper-split">
+                {/* Left split filter content */}
+                <div className="filter-wrapper-split-top">
+                    <FilterTabDropdown data={{ content: "All" }}></FilterTabDropdown>
+                    <SearchInput></SearchInput>
+                </div>
 
-            <div className="filter-contents-right small">
-                <Dropdown data={{label: "By provider", compact: true}}></Dropdown>
-                <Dropdown data={{label: "By genre", compact: true}}></Dropdown>
-                <SearchInput></SearchInput>
+                {/* Right split filter content */}
+                <div className="filter-wrapper-split-bottom"> 
+                    <Dropdown data={{ label: "By provider", compact: true }}></Dropdown>
+                    <Dropdown data={{ label: "By genre", compact: true }}></Dropdown>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
