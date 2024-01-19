874da4e5938c5e00a6766f47604eed709a893c45
CeoOfStoicism <nikola.lukov.2006@gmail.com>
2024-01-19 22:34:28 +0200

    Fixed nothing found text going on next line

--- zettahosting-task/src/components/core/GameContainer/GameContainer.jsx
+++ zettahosting-task/src/components/core/GameContainer/GameContainer.jsx
@@ -5,10 +5,10 @@ import "./GameContainer.scss"
 
 function GameContainer() {
     const { changeActiveFilterHandler,
-            changeActiveProviderHandler,
-            changeActiveGenreHandler,
-            changeActiveSearchQueryHandler,
-            gamesToShow } = useContext(CatalogContext);
+        changeActiveProviderHandler,
+        changeActiveGenreHandler,
+        changeActiveSearchQueryHandler,
+        gamesToShow } = useContext(CatalogContext);
 
     function resetFilterHandler() {
         changeActiveFilterHandler(null);
@@ -22,12 +22,16 @@ function GameContainer() {
     })
 
     return (
-        <div className="games-container">
-            {gamesElement.length > 0 ?
-                gamesElement :
-                <div className="nothing-found">
-                    <p className="nothing-found-paragraph">Nothing was found for this query. Please change the query.</p>
-                    <p className="nothing-found-reset" onClick={resetFilterHandler}>Reset filter</p>
+        <div className="games">
+            <div className="games-container">
+                {gamesElement.length > 0 && gamesElement}
+
+            </div>
+
+            {gamesElement.length == 0 &&
+                <div className="games-nothing-found">
+                    <p className="games-nothing-found-paragraph">Nothing was found for this query. Please change the query.</p>
+                    <p className="games-nothing-found-reset" onClick={resetFilterHandler}>Reset filter</p>
                 </div>}
         </div>
     )
